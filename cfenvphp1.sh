a3ca82ef3de213d6f1eec52857fdab65c8a53dcb
Weien Chen <cenbiqing@gmail.com>
2023-03-23 04:43:14 +0000

    first commit

---
+++ cfenvphp1.sh
@@ -0,0 +1,8 @@
+export VCAP_SERVICES='{"p.mysql": [
+    {
+      "credentials": {"uri": "mysql://localhost:3306/service_instance_db?user=1ed4996b3eee490594e452860e01c14d&password=e0i82ip3oaafya6l&useSSL=false"
+      },
+      "name": "mydb",
+      "tags":["mysql"]
+    }
+  ]}'
